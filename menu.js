8096eb8f996415d5c28548a1fa1014474458132e
ArmParanyan <aparanyan472@gmail.com>
2023-10-26 10:29:13 +0400

    fetching for posts

---
+++ menu.js
@@ -0,0 +1,148 @@
+const navBar = document.getElementById("nav-bar");
+
+fetch("app.json")
+	.then(response => response.json())
+	.then(data => {
+		drawMenu(data);
+	})
+	.catch(error => {
+		console.error("Error fetching JSON", error);
+	});
+
+function drawMenu(data) {
+	const ul = document.createElement("ul");
+
+	data.forEach(item => {
+		const li = document.createElement("li");
+		const a = document.createElement("a");
+		li.innerHTML = item.group;
+		// a.classList.add("decoration")
+
+
+		li.appendChild(a);
+		ul.appendChild(li);
+
+		// const arrayProperties = Object.keys(item).filter(prop => Array.isArray(item[prop]));
+
+		// console.log(arrayProperties);
+
+		// console.log(CHILDREN);
+
+		item.pages.forEach(elem => {
+			const pagesLi = document.createElement("li");
+			const pagesA = document.createElement("a");
+			const pagesUl = document.createElement("ul");
+			const icon = document.createElement("img");
+			const arrowIcon = document.createElement("img");
+
+			pagesA.classList.add("decoration");
+
+			pagesA.innerHTML = elem.title;
+			// console.log(elem.title);
+			pagesA.href = elem.path;
+			icon.src = elem.icon;
+
+			pagesLi.appendChild(icon);
+			pagesLi.appendChild(pagesA);
+
+
+			pagesUl.appendChild(pagesLi);
+
+
+			if (elem.children && elem.children.length > 0) {
+				arrowIcon.src = "./images/ic_Chevron.svg";
+				arrowIcon.style.transform = "rotate(90deg)";
+
+				pagesLi.appendChild(arrowIcon);
+
+				// drawMenu(elem.children);
+
+				// console.log(elem.children)
+
+				// elem.children.forEach(child => {
+				// 	console.log("       ",child.title);
+				// });
+
+			}
+
+
+			ul.appendChild(pagesUl);
+
+
+		});
+
+
+		// drawMenu(item, navBar);
+
+
+	});
+
+	navBar.appendChild(ul);
+
+
+}
+
+
+// const navBar = document.getElementById("nav-bar");
+//
+// fetch("app.json")
+// 	.then((response) => response.json())
+// 	.then((data) => {
+// 		drawMenu(data);
+// 	})
+// 	.catch((error) => {
+// 		console.error("Error fetching JSON", error);
+// 	});
+//
+// function drawMenu(data) {
+// 	const ul = document.createElement("ul");
+//
+// 	data.forEach((item) => {
+// 		const li = document.createElement("li");
+// 		const a = document.createElement("a");
+// 		li.innerHTML = item.group;
+//
+//
+// 		li.appendChild(a);
+// 		ul.appendChild(li);
+// 		// li.style.borderTop = "1px solid #E9ECEF";
+//
+// 		for (const prop in item) {
+// 			if (Array.isArray(item[prop])) {
+// 				item[prop].forEach((elem) => {
+// 					const pagesLi = document.createElement("li");
+// 					const pagesA = document.createElement("a");
+// 					const pagesUl = document.createElement("ul");
+// 					const icon = document.createElement("img");
+// 					const arrowIcon = document.createElement("img");
+//
+// 					pagesA.classList.add("decoration");
+// 					pagesA.innerHTML = elem.title;
+// 					pagesA.href = elem.path;
+// 					icon.src = elem.icon;
+//
+// 					pagesLi.appendChild(icon);
+// 					pagesLi.appendChild(pagesA);
+// 					pagesUl.appendChild(pagesLi);
+//
+// 					if (elem.children && elem.children.length > 0) {
+// 						arrowIcon.src = "./images/ic_Chevron.svg";
+// 						arrowIcon.style.transform = "rotate(90deg)";
+// 						pagesLi.appendChild(arrowIcon);
+// 						// elem.children.forEach((child) => {
+// 						// 	console.log(child.title);
+// 						// });
+//
+// 						// drawMenu(elem.children);
+// 					}
+//
+// 					ul.appendChild(pagesUl);
+// 				});
+// 			}
+// 		}
+// 	});
+//
+// 	navBar.appendChild(ul);
+// }
+
+// console.log(navBar.offsetHeight);
--- script.js
+++ script.js
@@ -1,54 +1,140 @@
-const navBar = document.getElementById("nav-bar");
 
-fetch("app.json")
-	.then(response => response.json())
-	.then(data => {
-		drawMenu(data);
-	})
-	.catch(error => {
-		console.error("Error fetching JSON", error);
-	});
+function drawPosts() {
+	let html = `  <div class="posts-and-button-wrapper">
+                <div class="posts-text">
+                    <p>Posts</p>
+                </div>
 
-function drawMenu(data) {
-	const ul = document.createElement("ul");
+                <div class="create-buttons-wrapper">
+                    <button class="create-buttons">Create</button>
+                </div>
 
-	data.forEach(group => {
-		const groupLi = document.createElement("li");
-		groupLi.textContent = group.group;
+            </div>
 
-		const pageUl = document.createElement("ul");
+            <div class="posts-main-part">
+                <div class="posts">
+                    <div class="pound-sign pad">
+                        <p class="about-users-text">#</p>
+                    </div>
 
-		group.pages.forEach(page => {
-			if (page.children.length > 0) {
-				const pageLi = document.createElement("li");
+                    <div class="user-name pad">
+                        <p class="about-users-text">User Name</p>
+                    </div>
+
+                    <div class="title pad">
+                        <p class="about-users-text">Title</p>
+                    </div>
+
+                    <div class="actions pad">
+                        <p class="about-users-text">Actions</p>
+                    </div>
+                </div>
+            </div> `;
 
 
-				//image porc TODO
-				const image = document.createElement('img');
-				image.src = "./images/Beared%20Guy02-min%201.png";
-				pageLi.textContent = page.title;
+	const postWrapper = document.querySelector(".posts-wrapper");
+
+	postWrapper.innerHTML = html;
+}
+
+drawPosts();
 
 
-				const childrenUl = document.createElement("ul");
+// const navBarHeight = document.getElementById("#nav-bar");
+//
+// console.log(navBarHeightr.offsetHeight);
 
-				page.children.forEach(child => {
-					const childLi = document.createElement("li");
-					childLi.textContent = child.title;
-					childrenUl.appendChild(childLi);
-				});
 
-				pageLi.appendChild(childrenUl);
-				pageLi.appendChild(image)
-				pageUl.appendChild(pageLi);
+
+// function drawUserFromFetching() {
+// 	let html = `
+// 		<div className="user-datas">
+// 			<div className="user-id">
+//
+// 			</div>
+// 			<div className="user-name">
+//
+// 			</div>
+// 			<div className="title">
+//
+// 			</div>
+// 			<div className="changes">
+//
+// 			</div>
+// 		</div>
+// 	`
+// }
+
+
+
+const fetchPosts = fetch("https://jsonplaceholder.typicode.com/posts")
+	.then(response => response.json());
+
+const fetchUsers = fetch("https://jsonplaceholder.typicode.com/users")
+	.then(response => response.json());
+
+Promise.all([fetchPosts, fetchUsers])
+	.then(([postsData, usersData]) => {
+		const userIds = usersData.map(user => user.id);
+		// console.log(userIds);
+		const postIds = postsData.map(posts => posts.userId);
+		// console.log(postIds);
+
+		const commonIds = userIds.filter(userId => postIds.includes(userId));
+
+		const postsWrapper = document.querySelector(".posts-wrapper");
+
+		commonIds.forEach((commonId, index) => {
+			const post = postsData.find(post => post.userId === commonId);
+			const user = usersData.find(user => user.id === commonId);
+
+			if (post && user) {
+				const userDatas = document.createElement("div");
+				// const postsMainPart = document.createElement("div");
+
+				// postsMainPart.classList.add('posts-main-part');
+
+				const postsMainPart = document.querySelector(".posts-main-part")
+
+				userDatas.classList.add('user-datas', 'pad');
+
+				if (index % 2 !== 0) {
+					userDatas.style.background = "#F5F6FA"
+				} else {
+					userDatas.style.background = "#FFF"
+
+				}
+
+
+				const userId = document.createElement("div");
+				userId.classList.add("user-id", 'pad');
+
+				userId.textContent = index + 1;
+
+
+				const userName = document.createElement('div');
+
+				userName.classList.add('user-name', 'pad');
+				userName.textContent = user.username;
+
+				const title = document.createElement('div');
+				title.classList.add('title', 'pad');
+				title.textContent = post.title;
+
+				const changes = document.createElement('div');
+				changes.classList.add('changes');
+
+				userDatas.appendChild(userId);
+				userDatas.appendChild(userName);
+				userDatas.appendChild(title);
+				userDatas.appendChild(changes);
+				postsMainPart.appendChild(userDatas);
+				postsWrapper.appendChild(postsMainPart);
+
 			}
 		});
 
-		if (pageUl.childNodes.length > 0) {
-			groupLi.appendChild(pageUl);
-			ul.appendChild(groupLi);
-		}
-
-	});
-	navBar.appendChild(ul);
-}
-
+	})
+	.catch(err => {
+		console.error("Something went wrong", err);
+	})
